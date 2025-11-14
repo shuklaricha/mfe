6ef2f8e1b22609e0377ba28f81a659c37ba6469d
shuklaricha <shukla.richa068@gmail.com>
2025-11-14 12:13:37 +0530

    added css distinguher

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
-import { StylesProvider } from '@material-ui/core';
+import React from "react";
+import {
+  Switch,
+  Route,
+  BrowserRouter,
+} from "react-router-dom/cjs/react-router-dom.min";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
-import  Pricing  from './components/Pricing';
-import  Landing  from './components/Landing';
+import Pricing from "./components/Pricing";
+import Landing from "./components/Landing";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
 
 export default () => {
-    return <div>
-        <StylesProvider>
-            <BrowserRouter>
-            <Switch>
-                <Route exact path="/pricing" component = {Pricing}></Route>
-                <Route path="/" component = {Landing}></Route>
-            </Switch>
-            </BrowserRouter>
-        </StylesProvider>
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing}></Route>
+            <Route path="/" component={Landing}></Route>
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
-}
+  );
+};
